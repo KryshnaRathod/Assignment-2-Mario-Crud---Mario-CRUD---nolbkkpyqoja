d3561d1619ba5fcd58d6a65aa32e92423c188d2b
unknown <krishna.rathod227@gmail.com>
2022-02-08 11:37:16 +0530

    first-commit

--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+const marioSchema = new mongoose.Schema({
+    name: String,
+    weight: Number,
+});
 
-//  Your code goes here
-
+const marioModel = mongoose.model("mariochar", marioSchema);
 
 module.exports = marioModel;
+
+
